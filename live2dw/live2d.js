cfcb186f787cca39db7fc8da6e7168a26f6eb370
yijian <373400621@qq.com>
2025-07-26 09:32:19 +0800

    gh-pages ci/cd

--- live2dw/live2d.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-      setTimeout(() => {
-        L2Dwidget.init({pluginRootPath:"live2dw/",pluginJsPath:"lib/",pluginModelPath:"assets/",tagMode:!1,debug:!1,model:{jsonPath:"/live2dw/assets/koharu.model.json"},display:{position:"right",width:150,height:300},mobile:{show:!1},log:!1});
-      }, 250); // delay reload 1 seconds
-    });
